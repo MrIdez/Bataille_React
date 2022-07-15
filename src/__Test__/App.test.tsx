8a63227c94a60d71b4c5720d090517689ebb5755
Guillaume <baron.guillaume44@gmail.com>
2022-07-15 13:47:03 +0200

    Passage de L'application sur Vite.js

--- src/__Test__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-
-import { render, screen } from '@testing-library/react';
-import App from '../Components/App';
-
-test('renders Bienvenue', () => {
-  render(<App />);
-  const BienvenueElement = screen.getByText(/Bienvenue/i);
-  expect(BienvenueElement).toBeInTheDocument();
-});
